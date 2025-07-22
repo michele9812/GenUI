092f3aaaa064cae2ab13c63f0f29992304ff2f52
michele9812 <125456585+michele9812@users.noreply.github.com>
2025-07-22 16:05:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/GenUI/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
